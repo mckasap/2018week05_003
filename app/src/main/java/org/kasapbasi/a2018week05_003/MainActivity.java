3e00706b8ad06e4e9b07b7007fabd05972b48051
mckasap <mckasap@gmail.com>
2018-03-15 11:36:34 +0300

    Creating SQLite Database

--- app/src/main/java/org/kasapbasi/a2018week05_003/MainActivity.java
+++ app/src/main/java/org/kasapbasi/a2018week05_003/MainActivity.java
@@ -1,5 +1,6 @@
 package org.kasapbasi.a2018week05_003;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase db= openOrCreateDatabase("Test",MODE_PRIVATE,null);
+
+        db.execSQL("Create Table if not exists " +
+                "MyTable(" +
+                "Id INTEGER PRIMARY KEY AUTOINCREMENT ," +
+                "Lastname varchar, " +
+                "FirstName CHAR(50),  " +
+                "Age int(3));");
+
+
+
+
     }
 }
