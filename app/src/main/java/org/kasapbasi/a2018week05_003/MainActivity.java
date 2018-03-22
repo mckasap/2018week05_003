5a76277d4e2a559da9f7939661f61c90eb673bd4
mckasap <mckasap@gmail.com>
2018-03-22 10:26:59 +0300

    // kaldığımız yer

--- app/src/main/java/org/kasapbasi/a2018week05_003/MainActivity.java
+++ app/src/main/java/org/kasapbasi/a2018week05_003/MainActivity.java
@@ -1,8 +1,10 @@
 package org.kasapbasi.a2018week05_003;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 // Acıklama Satırı :) 
@@ -13,20 +15,23 @@ public class MainActivity extends AppCompatActivity {
 
         SQLiteDatabase db= openOrCreateDatabase("Test",MODE_PRIVATE,null);
 
-        db.execSQL("Create Table if not exists " +
+       // db.execSQL("Drop Table MyTable");
+        db.execSQL("Create Table If not exists " +
                 "MyTable(" +
-                "Id INTEGER PRIMARY KEY AUTOINCREMENT ," +
-                "Lastname varchar, " +
-                "FirstName CHAR(50),  " +
-                "Age int(3));");
+                "Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                " Lastname varchar NOT NULL, " +
+                " FirstName CHAR(50) NOT NULL,  " +
+                " Age int(3) NOT NULL);");
 
-        db.execSQL("insert  into MyTable Values('kasapbasi', 'mustafa', 61);");
+        db.execSQL("insert  into MyTable(Lastname, FirstName, Age ) Values('kasapbasi', 'mustafa', 61);");
 
         db.close();
+
         db= openOrCreateDatabase("Test",MODE_PRIVATE,null);
        // test
+        Cursor c= db.rawQuery("Select * from MyTable",null);
+        Log.i("DATABASE",c.getColumnCount()+"");
 
-
-
+        db.close();
     }
 }
